1fbef51359b33e7f21f0ea7d5f3022cdeeafcbef
Zubova Katya <zubova03@mail.ru>
2023-12-02 13:00:30 +0300

    перекрашена кнопка

--- Klient_glav.Designer.cs
+++ Klient_glav.Designer.cs
@@ -147,12 +147,13 @@ namespace Bank
             // 
             // button5
             // 
+            this.button5.BackColor = System.Drawing.SystemColors.Info;
             this.button5.Location = new System.Drawing.Point(810, 53);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(119, 30);
             this.button5.TabIndex = 11;
             this.button5.Text = "выйти";
-            this.button5.UseVisualStyleBackColor = true;
+            this.button5.UseVisualStyleBackColor = false;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // btnhistoryDeposit
