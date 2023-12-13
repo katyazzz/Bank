122a28697b412e35f184c478c8cd3f189a69a955
Zubova Katya <zubova03@mail.ru>
2023-12-14 01:44:58 +0300

    11/24

--- CreateKlient.cs
+++ CreateKlient.cs
@@ -30,32 +30,56 @@ namespace Bank
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Klient_Creation.Parameters["@fio"].Value = txtfio.Text;
-            Klient_Creation.Parameters["@PasSeries"].Value = Convert.ToInt32(txtspas.Text);
-            Klient_Creation.Parameters["@PasNumber"].Value = Convert.ToInt32(txtnpas.Text);
-            Klient_Creation.Parameters["@bd"].Value = txtbd.Value;
-            Klient_Creation.Parameters["@sex"].Value =txtsex.Text;
-            // открыть соединение с БД
-            Connect.Open();
-            // выполнить sql-выражение (хранимую процедуру) и вернуть количество измененных записей
-            Klient_Creation.ExecuteNonQuery();
-            // закрыть соединение с БД
-            Connect.Close();
-            // вывести значение выходного параметра
-            ///int result = (int)Klient_Creation.Parameters["@id"].Value;
-            ///txtid.Text = Convert.ToString(result); // на форму
-                                                          // вывод результата через окно сообщения
-            MessageBox.Show("Пользователь добавлен!", "Успешно", MessageBoxButtons.OK,
-            MessageBoxIcon.Information);
-            // examBindingSource.AddNew();
-            this.Close();
+
+            try
+            {
+                int age = CalculateAge(txtbd.Value);
+
+                if (age >= 14)
+                {
+                    Klient_Creation.Parameters["@fio"].Value = txtfio.Text;
+                    Klient_Creation.Parameters["@PasSeries"].Value = Convert.ToInt32(txtspas.Text);
+                    Klient_Creation.Parameters["@PasNumber"].Value = Convert.ToInt32(txtnpas.Text);
+                    Klient_Creation.Parameters["@bd"].Value = txtbd.Value;
+                    Klient_Creation.Parameters["@sex"].Value = txtsex.Text;
+
+                    // Открыть соединение с БД
+                    Connect.Open();
+                    // Выполнить SQL-выражение (хранимую процедуру) и вернуть количество измененных записей
+                    Klient_Creation.ExecuteNonQuery();
+                    // Закрыть соединение с БД
+                    Connect.Close();
+
+                    MessageBox.Show("Пользователь добавлен!", "Успешно", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Клиенту должно быть не менее 14 лет.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Произошла ошибка: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
+        private int CalculateAge(DateTime birthdate)
+        {
+            DateTime currentDate = DateTime.Now;
+            int age = currentDate.Year - birthdate.Year;
 
+            if (currentDate.Month < birthdate.Month || (currentDate.Month == birthdate.Month && currentDate.Day < birthdate.Day))
+            {
+                age--;
+            }
+
+            return age;
+        }
         private void CreateKlient_Load(object sender, EventArgs e)
         {
             // TODO: данная строка кода позволяет загрузить данные в таблицу "bDBankDataSet.DepositPayment". При необходимости она может быть перемещена или удалена.
