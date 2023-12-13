3f27044167213482f4eb333b021eb0ff247a1732
Zubova Katya <zubova03@mail.ru>
2023-12-14 02:00:52 +0300

    12/24

--- RefillForm.cs
+++ RefillForm.cs
@@ -42,23 +42,32 @@ namespace Bank
       
         private void btnConfirm_Click_1(object sender, EventArgs e)
         {
-            // Получение суммы для пополнения из текстового поля
-            if (float.TryParse(txtAmount.Text, out float amount))
+            try
             {
-                // Получаем информацию о сотруднике
-                (int, string, string) staffInfo = db.GetStaffInfoByID(ID_Staff);
+                // Получение суммы для пополнения из текстового поля
+                if (float.TryParse(txtAmount.Text, out float amount))
+                {
+                    // Получаем информацию о сотруднике
+                    (int, string, string) staffInfo = db.GetStaffInfoByID(ID_Staff);
 
-                int staffID = staffInfo.Item1;
+                    int staffID = staffInfo.Item1;
 
-                // Вызов процедуры для пополнения счета
-                db.DepositMoney(accountNumber, amount, staffID);
-                // Закрываем форму пополнения после успешного выполнения операции
-                MessageBox.Show("Счет успешно пополнен!", "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                this.Close();
+                    // Вызов процедуры для пополнения счета
+                    db.DepositMoney(accountNumber, amount, staffID);
+
+                    // Если код дошел до этого момента, значит операция прошла успешно
+                    MessageBox.Show("Счет успешно пополнен!", "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Введите корректную сумму для пополнения.");
+                }
             }
-            else
+            catch (Exception ex)
             {
-                MessageBox.Show("Введите корректную сумму для пополнения.");
+                // Если произошло исключение, то, вероятно, счет закрыт
+                MessageBox.Show($"Ошибка при попытке пополнения счета: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
    
