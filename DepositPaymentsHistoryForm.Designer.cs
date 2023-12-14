157358dc1fd7f1f08d0aa4229ff9ca3da7779d8f
Zubova Katya <zubova03@mail.ru>
2023-12-14 21:39:20 +0300

    16/24

--- DepositPaymentsHistoryForm.Designer.cs
+++ DepositPaymentsHistoryForm.Designer.cs
@@ -30,39 +30,63 @@
         {
             this.dataGridViewDepositPaymentsHistory = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxDepositNumber = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewDepositPaymentsHistory)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridViewDepositPaymentsHistory
             // 
             this.dataGridViewDepositPaymentsHistory.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewDepositPaymentsHistory.Location = new System.Drawing.Point(34, 128);
-            this.dataGridViewDepositPaymentsHistory.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridViewDepositPaymentsHistory.Location = new System.Drawing.Point(41, 112);
+            this.dataGridViewDepositPaymentsHistory.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dataGridViewDepositPaymentsHistory.Name = "dataGridViewDepositPaymentsHistory";
             this.dataGridViewDepositPaymentsHistory.RowTemplate.Height = 24;
-            this.dataGridViewDepositPaymentsHistory.Size = new System.Drawing.Size(608, 174);
+            this.dataGridViewDepositPaymentsHistory.Size = new System.Drawing.Size(628, 201);
             this.dataGridViewDepositPaymentsHistory.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(31, 30);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(36, 30);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(208, 25);
             this.label1.TabIndex = 1;
             this.label1.Text = "История выплат";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(38, 75);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Поиск по вкладу:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBoxDepositNumber
+            // 
+            this.textBoxDepositNumber.Location = new System.Drawing.Point(173, 75);
+            this.textBoxDepositNumber.Name = "textBoxDepositNumber";
+            this.textBoxDepositNumber.Size = new System.Drawing.Size(130, 21);
+            this.textBoxDepositNumber.TabIndex = 5;
+            this.textBoxDepositNumber.TextChanged += new System.EventHandler(this.textBoxDepositNumber_TextChanged);
+            // 
             // DepositPaymentsHistoryForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FloralWhite;
-            this.ClientSize = new System.Drawing.Size(685, 366);
+            this.ClientSize = new System.Drawing.Size(711, 366);
+            this.Controls.Add(this.textBoxDepositNumber);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridViewDepositPaymentsHistory);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "DepositPaymentsHistoryForm";
             this.Text = "История выплат";
             this.Load += new System.EventHandler(this.DepositPaymentsHistoryForm_Load);
@@ -76,5 +100,7 @@
 
         private System.Windows.Forms.DataGridView dataGridViewDepositPaymentsHistory;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxDepositNumber;
     }
 }
--- DepositPaymentsHistoryForm.cs
+++ DepositPaymentsHistoryForm.cs
@@ -13,13 +13,31 @@ namespace Bank
 {
     public partial class DepositPaymentsHistoryForm : Form
     {
+        private BindingSource bindingSource;
+        private DatabaseHelper db;
+        private int pasSeries;
+        private int pasNumber;
+        public DepositPaymentsHistoryForm(int pasSeries, int pasNumber, DatabaseHelper db)
+        {
+            InitializeComponent();
+            bindingSource = new BindingSource();
+            this.db = db;
+            this.pasSeries = pasSeries;
+            this.pasNumber = pasNumber;
+
+            dataGridViewDepositPaymentsHistory.DataSource = bindingSource;
+        }
+        
         public DepositPaymentsHistoryForm()
         {
             InitializeComponent();
+            bindingSource = new BindingSource();
+
         }
 
         public void LoadDepositPaymentsHistory(int pasSeries, int pasNumber, DatabaseHelper db)
         {
+
             try
             {
                 using (SqlConnection connection = new SqlConnection(db.GetConnectionString()))
@@ -36,7 +54,16 @@ namespace Bank
                         DataTable dataTable = new DataTable();
                         adapter.Fill(dataTable);
 
-                        dataGridViewDepositPaymentsHistory.DataSource = dataTable.DefaultView.ToTable(false, "Номер Операции", "Номер Вклада", "Дата", "Сумма", "Остаток");
+                        // Проверяем наличие столбца "Номер Вклада" в таблице
+                        if (dataTable.Columns.Contains("Номер Вклада"))
+                        {
+                            bindingSource.DataSource = dataTable;
+                            dataGridViewDepositPaymentsHistory.DataSource = bindingSource;
+                        }
+                        else
+                        {
+                            MessageBox.Show("Столбец 'Номер Вклада' не найден в таблице данных.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
                     }
                 }
             }
@@ -46,9 +73,46 @@ namespace Bank
             }
         }
 
+        private void dataGridViewDepositPaymentsHistory_ColumnHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            // Обработка щелчка по заголовку столбца для сортировки
+            DataGridViewColumn newColumn = dataGridViewDepositPaymentsHistory.Columns[e.ColumnIndex];
+            string propertyName = newColumn.DataPropertyName;
+
+            // Смена направления сортировки при повторном щелчке
+            ListSortDirection direction = ListSortDirection.Ascending;
+            if (bindingSource.Sort != null && bindingSource.Sort.Contains(propertyName))
+                direction = (bindingSource.SortDirection == ListSortDirection.Ascending)
+                    ? ListSortDirection.Descending
+                    : ListSortDirection.Ascending;
+
+            // Применение сортировки
+            bindingSource.Sort = $"{propertyName} {GetSortDirectionString(direction)}";
+
+        }
+
+        private string GetSortDirectionString(ListSortDirection direction)
+        {
+            return direction == ListSortDirection.Ascending ? "ASC" : "DESC";
+        }
+
         private void DepositPaymentsHistoryForm_Load(object sender, EventArgs e)
         {
 
         }
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxDepositNumber_TextChanged(object sender, EventArgs e)
+        {
+            string filterExpression = $"[Номер Вклада] = '{textBoxDepositNumber.Text}'";
+            bindingSource.Filter = filterExpression;
+
+            // Отладочное сообщение
+            MessageBox.Show($"Applied filter: {filterExpression}");
+        }
+
     }
 }
